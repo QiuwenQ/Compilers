b26cc496984b26f881f2709e183824c4ade11573
QiuwenQ <qquan2@wisc.edu>
2020-03-23 23:10:57 -0500

    struct symtable

--- p4/Sym.java
+++ p4/Sym.java
@@ -1,8 +1,20 @@
 public class Sym {
     private String type;
+
+    /* table is used to separately store the symbol table 
+    associated with each struct definition */
+    private HashMap<String, Sym> table;
     
     public Sym(String type) {
         this.type = type;
+    }
+    //method to set the struct symbol table
+    public setTable(HashMap<String, Sym> tab){
+        this.table = tab;
+    }
+    //accessor method for struct symbol table
+    public getTable(){
+        return this.table;
     }
     
     public String getType() {
